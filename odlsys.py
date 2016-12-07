e6232cf90ce8e3c0289f5d2624c8248d2c0d1f49
administrator <administrator@BSC40-183.bracknell.rtse.brocade.com>
2016-12-07 04:56:22 -0800

    removed reading /proc/meminfo directly

--- odlsys.py
+++ odlsys.py
@@ -17,26 +17,7 @@ def check():
 
     # 2. Check memory 'free -h' or /proc/meminfo
     testTools.name("MEMORY")
-    #sysRun("free -h")
-
-    ### Open the /proc/meminfo file for parsing
-    meminfo_path = "/proc/meminfo"
-    input = open(meminfo_path, "r")
-    meminfo = input.read()
-    input.close()
-
-    ### Parse the meminfo file
-    meminfo_list = meminfo.split('\n')
-    mem_total = 0
-    swap_total = 0
-    for i in range(len(meminfo_list)):
-        if( "MemTotal" in meminfo_list[i] ):
-            mem_total += int(meminfo_list[i].split()[1])
-        if( "SwapTotal" in meminfo_list[i] ):
-            swap_total += int(meminfo_list[i].split()[1])
-
-    print("{0:{width}}".format("Total Memory: ", width=100) , testTools.humanBytes(mem_total))
-    print("{0:{width}}".format("Total Swap: ", width=100)   , testTools.humanBytes(swap_total))
+    sysRun("free -h")
 
     # 3. Checking Disk  with 'df -ahl --total' or /proc/diskstats
     testTools.name("DISK")
