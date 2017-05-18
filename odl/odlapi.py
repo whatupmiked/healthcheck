12c52b535bfce65db13183c071e831e37dba8ef0
whatupmiked <whatupmiked@gmail.com>
2017-05-18 12:17:48 +0100

    First pass through linter

--- odl/odlapi.py
+++ odl/odlapi.py
@@ -1,26 +1,30 @@
+"""
+Interpret the ODL apis
+"""
 import requests
 
 # Headers
-j_headers = {"Accept": "application/json", "Content-type": "application/json"}
+J_HEADERS = {"Accept": "application/json", "Content-type": "application/json"}
 
-apis = "apidoc/apis/"
+APIS = "apidoc/apis/"
 
 def api_list(**args):
-    """Given controller access details, returns a list of api paths"""
-    """curl -u admin:admin http://<controller_ip>:8181/apidoc/apis/"""
-    api_uri = "http://{0}:8181/{1}".format(args['controller_ip'],apis)
+    """
+    Given controller access details, returns a list of api paths
+    curl -u admin:admin http://<controller_ip>:8181/apidoc/apis/
+    """
+    api_uri = "http://{0}:8181/{1}".format(args['controller_ip'], APIS)
 
     #print("Printing API list from {0}".format(api_uri), end='\n\n')
 
-    ra = requests.get(
-            api_uri,
-            auth=(args['username'],args['password']),
-            headers=j_headers)
+    r_a = requests.get(
+        api_uri,
+        auth=(args['username'], args['password']),
+        headers=J_HEADERS)
 
-    api_list = []
-    api_json = ra.json()
+    api_l = []
+    api_json = r_a.json()
     for i in range(len(api_json['apis'])):
-        api_list.append(api_json['apis'][i]['path'])
+        api_l.append(api_json['apis'][i]['path'])
 
-    print( *api_list, sep='\n' )
-
+    print(*api_l, sep='\n')
